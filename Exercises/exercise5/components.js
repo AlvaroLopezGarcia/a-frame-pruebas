94fcc86a0ac196902cd006ae52e39b4864ec59a0
AlvaroLopezGarcia <alvarito2339@gmail.com>
2019-12-14 13:55:50 +0100

    Creado menu de programa,corregida altura de las instrucciones al eliminarlas,cambiado el cursor e implementado el boton 'Delete Program'

--- Exercises/exercise5/components.js
+++ Exercises/exercise5/components.js
@@ -16,9 +16,9 @@ AFRAME.registerComponent('programmer_component', {
 
     let box = document.createElement('a-box');
     document.getElementById("programmer").appendChild(box);
-    box.setAttribute('position',{x:0.5,y:0.5,z:-6});
+    box.setAttribute('position',{x:0.5,y:2.5,z:-5.85});
     box.setAttribute('color',"brown");
-    box.setAttribute('geometry', {width: '1',height: "1",depth: "1.7"});
+    box.setAttribute('geometry', {width: '3',height: "5",depth: "0.5"});
   },
 
 });
@@ -34,7 +34,7 @@ AFRAME.registerComponent('mobile_component', {
 
     this.eventMobileHandlerMouseDown = function (){
       let program_id = this.getAttribute("mobile_component").program;
-      let instructions = document.getElementById(program_id).children[5].children;
+      let instructions = document.getElementById(program_id).children[6].children;
 
       for (let instruction of instructions) {
         instruction.emit('run');
@@ -106,46 +106,52 @@ AFRAME.registerComponent('button', {
  
     this.eventButtonHandlerUp = function () {
       //e.stopPropagation();
+      let instruction = document.createElement('a-entity');
       let box = document.createElement('a-box');
-      document.getElementById("instructions").appendChild(box);
       let programmer = document.getElementById("programmer");
       let num = programmer.getAttribute('programmer_component').count;
-      let pos = programmer.children[6].getAttribute("position");
-      let pos_x = pos.x + 2;
+      let pos = programmer.children[7].getAttribute("position");
+      let pos_x = pos.x + 5;
       let pos_y = pos.y;
       let incremento = 1;
       let instruction_id = "instruction" + num;
 
+      document.getElementById("instructions").appendChild(instruction);
+      instruction.appendChild(box);
       if(num !== 0){
         pos_y += num + incremento*num;
       }
       num += incremento;
       programmer.setAttribute('programmer_component', {count: num});
       box.setAttribute('position',{x:pos_x,y:pos_y,z:pos.z});
-      box.setAttribute('id',instruction_id);
-      box.setAttribute('instruction_component', {event: 'run',type:'Up'});
+      box.setAttribute('geometry', {width: '1',height: "0.5",depth: "0.5"});
+      instruction.setAttribute('id',instruction_id);
+      instruction.setAttribute('instruction_component', {event: 'run',type:'Up'});
     };
 
     this.eventButtonHandlerDown = function () {
       //e.stopPropagation();
+      let instruction = document.createElement('a-entity');
       let box = document.createElement('a-box');
-      document.getElementById("instructions").appendChild(box);
       let programmer = document.getElementById("programmer");
       let num = programmer.getAttribute('programmer_component').count;
-      let pos = programmer.children[6].getAttribute("position");
-      let pos_x = pos.x + 2;
+      let pos = programmer.children[7].getAttribute("position");
+      let pos_x = pos.x + 5;
       let pos_y = pos.y;
       let incremento = 1;
       let instruction_id = "instruction" + num;
 
+      document.getElementById("instructions").appendChild(instruction);
+      instruction.appendChild(box);
       if(num !== 0){
         pos_y += num + incremento*num;
       }
       num += incremento;
       programmer.setAttribute('programmer_component', {count: num});
       box.setAttribute('position',{x:pos_x,y:pos_y,z:pos.z});
-      box.setAttribute('id',instruction_id);
-      box.setAttribute('instruction_component', {event: 'run',type:'Down'});
+      box.setAttribute('geometry', {width: '1',height: "0.5",depth: "0.5"});
+      instruction.setAttribute('id',instruction_id);
+      instruction.setAttribute('instruction_component', {event: 'run',type:'Down'});
     };
 
     //Finds all the mobiles with a specific program id
@@ -160,14 +166,23 @@ AFRAME.registerComponent('button', {
       }
     };
 
-    this.eventButtonHandlerDelete = function () {
-      let instructions = document.getElementById("programmer").children[5];
+    this.eventButtonHandlerDeleteInstructions = function () {
+      let instructions = document.getElementById("programmer").children[6];
       let child = instructions.lastElementChild;
+      let programmer = document.getElementById("programmer");
 
       while (child) { 
         instructions.removeChild(child); 
         child = instructions.lastElementChild; 
       }
+      programmer.setAttribute('programmer_component', {count: 0});
+    };
+
+    this.eventButtonHandlerDeleteProgram = function () {
+      let ide = document.getElementById('programming-enviroment');
+      let program = this.parentNode;
+
+      ide.removeChild(program);
     };
 
     if(this.data.text === 'Up'){
@@ -176,9 +191,10 @@ AFRAME.registerComponent('button', {
       this.el.addEventListener("click", this.eventButtonHandlerDown);
     }else if(this.data.text === 'Run'){
       this.el.addEventListener("click", this.eventButtonHandlerRun);
-    }else if(this.data.text === 'Delete'){
-      this.el.addEventListener("click", this.eventButtonHandlerDelete);
+    }else if(this.data.text === 'Delete Instructions'){
+      this.el.addEventListener("click", this.eventButtonHandlerDeleteInstructions);
+    }else if(this.data.text === 'Delete Program'){
+      this.el.addEventListener("click", this.eventButtonHandlerDeleteProgram);
     }
-    
   },
 });
